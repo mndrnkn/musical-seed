389c9abf122b7020c78f1812d2548ec65d198c52
Amanda <amandarankin@amandas-mbp.myfiosgateway.com>
2023-09-15 09:34:31 -0400

    added rhythm tracks

--- src/data/rhythmTrack.ts
+++ src/data/rhythmTrack.ts
@@ -21,7 +21,28 @@ export const rhythmTracks: CollectionType[] = [
         ],
       },
       {
-        collection: "Latin",
+        collection: 'Latin',
+        tracks:[
+          'Bolero',
+            'Bomba',
+            'Calypso',
+            'Cha Cha (2-3)',
+            'Clave 2-3',
+            'Clave 3-2',
+            'Clave (Rumba) 2-3',
+            'Clave (Rumba_ 3-2',
+            'Danzon (2-3)',
+            'Mambo (2-3)',
+            'Merengue (3-2)',
+            'Plena',
+            'Rumba Columbia (3-2)',
+            'Rumba Guaguanco (3-2)',
+            'Son (2-3)',
+            'Son Montuno (2-3)',
+        ]
+      },
+      {
+        collection: "Brazilian",
         tracks: [
           "Bossa Nova",
           "Bossa Samba",
@@ -63,4 +84,176 @@ export const rhythmTracks: CollectionType[] = [
             'Drum&Bass 10',
         ],
       },
+      {
+        collection: 'Oldies',
+        tracks:[
+          'Oldie 1 - 4 on snare',
+          'Oldie 2 - 4 on snare',
+          'Oldie 3 - 4 on snare',
+          'Oldie 4 - 4 Bum cha ka',
+          'Oldie 5- Bum cha ka more',
+          'Oldie 6'
+        ]
+      },
+      {
+        collection: 'Blues',
+        tracks: [
+          'Blues 1',
+          'Blues 2',
+          'Blues 3- 6/8',
+          'Blues 4- 6/8',
+          'Blues 5- 6/8',
+          'Blues 6- 6/8',
+          'Blues 7- 6/8',
+          'Blues 8- 12/8',
+          'Blues 9- 12/8',
+          'Blues 10- 12/8',
+        ]
+      },
+      {
+        collection:'Ballads',
+        tracks:[
+          'Ballad 2',
+          'Ballad 3',
+          'Ballad 6',
+          'Ballad 8',
+          'Ballad 13',
+        ]
+      },
+      {
+        collection: 'pop',
+        tracks:[
+          'Pop 1- 16ths',
+          'Pop 2- 16ths',
+          'Pop 3- 16ths',
+          'Pop 4- 16ths',
+          'Pop 5- 16ths',
+          'Pop 6- 8ths',
+          'Pop 7- anticipated 1',
+          'Pop 8- 8ths',
+          'Pop 9- 8ths',
+          'Pop 10- 8ths',
+          'Pop 11- 16ths',
+          'Pop 12- 8ths',
+        ]
+      },
+      {
+        collection: 'Hip Hop',
+        tracks: [
+          'Hip Hop 1',
+          'Hip Hop 2',
+          'Hip Hop 3',
+          'Hip Hop 4',
+          'Hip Hop 5',
+          'Hip Hop 6 - swung',
+          'Hip Hop 7 - swung',
+          'Hip Hop 8 - swung',
+          'Hip Hop 9 - swung',
+          'Hip Hop 10 - swung',
+        ]
+      },
+      {
+        collection: 'Rock',
+        tracks: [
+          'Rock 1',
+          'Rock 2',
+          'Rock 3',
+          'Rock 4',
+          'Rock 5',
+          'Rock 6',
+          'Rock 7',
+          'Rock 8',
+          'Rock 9',
+          'Rock 10',
+          'Rock 11',
+          'Rock 12',
+          'Rock 13',
+        ]
+      },
+      {
+        collection: 'odd time',
+        tracks:[
+          'Odd Time 1- 5/4',
+          'Odd Time 2- 5/8',
+          'Odd Time 3- 6/8',
+          'Odd Time 4- 6/8 feels like 3/4',
+          'Odd Time 5- 7/4',
+          'Odd Time 6- 7/8',
+          'Odd Time 7- 9/4',
+          'Odd Time 8- 9/8',
+          'Odd Time 9- 9/4 Ethno',
+          'Odd Time 10- 7/8'
+        ]
+      },
+      {
+        collection: 'funk',
+        tracks:[
+          'Funk 1- str 16ths',
+          'Funk 2- str 16ths',
+          'Funk 3- str 16ths',
+          'Funk 4- str 16ths',
+          'Funk 5- str 16ths',
+          'Funk 6- swung',
+          'Funk 7- swung',
+          'Funk 8- swung',
+          'Funk 9- swung',
+          'Funk 10- swung'
+        ]
+      },
+      {
+        collection: 'Hand percussion',
+        tracks:[
+          'Straight 8th',
+          'Straight 16th',
+          'Ballad',
+          'Shuffle Funk',
+          'Shuffle',
+        ]
+      },
+      {
+        collection:'jazz',
+        tracks:[
+          'Jazz 1- med swing',
+           'Jazz 2- med Basie swing',
+          'Jazz 3- med swing',
+          'Jazz 4- med swing walk',
+          'Jazz 5- med up swing',
+          'Jazz 6- med up complex',
+          'Jazz 7- up tempo Bebop',
+          'Jazz 8- modal open swing',
+          'Jazz 9- 3/4 swing',
+          'Jazz 10- Jazz eight',
+        ]
+      },
+      {
+        collection:'Brushes',
+        tracks:[
+       'Brushes 1- Str 8',
+        'Brushes 2- Str 16ths',
+        'Brushes 3- Half time shuffle',
+        'Brushes 4- 3/4 Shuffle Waltz',
+        'Brushes 5- 3/4 Half time Shuffle Pop',
+        'Brushes 6- 5/4 simple',
+        'Brushes 7- 7/8 simple',
+        'Brushes 8- Str 8',
+        'Brushes 9- Bossa Nova brushes',
+        'Brushes 10- Spanish Rumba',
+        ]
+      },
+      {
+        collection: 'world',
+        tracks: [
+          'Tango',
+          'Piazzolla',
+          'Polka',
+          'Polka var',
+          'Serbian Ethno',
+          'Waltz',
+          'Spanish Buleria',
+          'Spanish Rumba',
+          'Buleria w brushes',
+          'Spanish 3/4 beat- brushes',
+        ]
+      },
+     
 ];
